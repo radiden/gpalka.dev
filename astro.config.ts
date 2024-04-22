793dd4e16f14f7b41b9c0eb637fad0b0e0e423b0
radiden <gabriel@rai.cx>
2024-04-22 21:30:22 +0200

    initial

---
+++ astro.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+import unocss from 'unocss/astro'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		unocss({
+			injectReset: true
+		})
+	]
+})
---
+++ uno.config.ts
@@ -0,0 +1,26 @@
+// uno.config.ts
+import { defineConfig, presetIcons, presetWebFonts } from 'unocss'
+import { presetWind } from 'unocss'
+import { presetRadix } from 'unocss-preset-radix'
+import { transformerDirectives } from 'unocss'
+
+export default defineConfig({
+	presets: [
+		presetWind(),
+		presetRadix({
+			darkSelector: ':root',
+			lightSelector: '.never',
+			palette: ['mauve', 'pink', 'purple'],
+			aliases: {
+				primary: 'mauve',
+				accent: 'pink'
+			}
+		}),
+		presetIcons()
+	],
+	transformers: [
+		transformerDirectives({
+			applyVariable: ['--uno']
+		})
+	]
+})
